b8e76d11f37f6180e54c836ac2d264482267f69d
SCrawshaw18 <scott_crawshaw18@milton.edu>
2018-04-29 17:46:06 -0400

    heyo

--- SlangGenerator.py
+++ SlangGenerator.py
@@ -3,7 +3,6 @@ from decimal import Decimal
 options=str(subprocess.check_output(["ls cv"],shell=True)).split("\\n")
 del options[-1]
 options[0]=options[0][2:]
-print(options)
 best=3.0
 checkpoint=""
 for option in options:
@@ -12,7 +11,9 @@ for option in options:
 		error=best
 		checkpoint=option
 print("Checkpoint chosen: %s"%(checkpoint))
-word = input("Make up a word and enter it here: ") + ": "
-temperature = input("Set your desired temperature (0-1): ")
+word = input("Make up a word and enter it here: ") + ":"
+temperature = input("Set your desired temperature (0-1, 0.5 reccomended): ")
 response = str(subprocess.check_output(['''th sample.lua cv/%s -primetext "%s" -temperature %s'''%(checkpoint,word,temperature)],shell=True))
+response = response[response.rfind(word):]
+response = response[:response.find("\n")]
 print(response)
