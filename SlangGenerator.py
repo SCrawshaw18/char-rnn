2ff794b81165364b096afbcb97bd4142076cfbe1
SCrawshaw18 <scott_crawshaw18@milton.edu>
2018-04-29 17:39:32 -0400

    he

--- SlangGenerator.py
+++ SlangGenerator.py
@@ -7,12 +7,12 @@ print(options)
 best=3.0
 checkpoint=""
 for option in options:
-	print(option[option.rfind("_")+1:option.rfind(".")])
 	error=Decimal(option[option.rfind("_")+1:option.rfind(".")])
 	if error < best:
 		error=best
 		checkpoint=option
-word = input("Make up a word and enter it here: ")
+print("Checkpoint chosen: %s"%(checkpoint))
+word = input("Make up a word and enter it here: ") + ": "
 temperature = input("Set your desired temperature (0-1): ")
-response = str(subprocess.check_output(['''th sample.lua cv/%s -primetext "%s:" -temperature %s'''%(checkpoint,word,temperature)],shell=True))
+response = str(subprocess.check_output(['''th sample.lua cv/%s -primetext "%s" -temperature %s'''%(checkpoint,word,temperature)],shell=True))
 print(response)
