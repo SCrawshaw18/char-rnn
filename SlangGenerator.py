cf4ddc7c8817cf01149820239b99365a313cdcd8
SCrawshaw18 <scott_crawshaw18@milton.edu>
2018-04-29 17:36:22 -0400

    hey

--- SlangGenerator.py
+++ SlangGenerator.py
@@ -1,10 +1,13 @@
 import subprocess
 from decimal import Decimal
 options=str(subprocess.check_output(["ls cv"],shell=True)).split("\\n")
+del options[-1]
+options[0]=options[0][2:]
 print(options)
 best=3.0
 checkpoint=""
 for option in options:
+	print(option[option.rfind("_")+1:option.rfind(".")])
 	error=Decimal(option[option.rfind("_")+1:option.rfind(".")])
 	if error < best:
 		error=best
