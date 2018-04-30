9f50c4a8b68b77b3b692bea0cddc2cfa05bf77f1
SCrawshaw18 <scott_crawshaw18@milton.edu>
2018-04-30 15:52:07 -0400

    fixed

--- SlangGenerator.py
+++ SlangGenerator.py
@@ -1,7 +1,7 @@
 import subprocess
 import datetime
 from decimal import Decimal
-options=str(subprocess.check_output(["ls cv"],shell=True)).split("\\n")
+options=str(subprocess.check_output(["ls ~/char-rnn/cv"],shell=True)).split("\\n")
 del options[-1]
 options[0]=options[0][2:]
 best=3.0
@@ -14,13 +14,13 @@ for option in options:
 print("Checkpoint chosen: %s"%(checkpoint))
 word = input("Make up a word and enter it here: ")
 temperature = input("Set your desired temperature (0-1, 0.5 reccomended): ")
-response = str(subprocess.check_output(['''th sample.lua cv/%s -primetext "%s:" -temperature %s'''%(checkpoint,word,temperature)],shell=True))
+response = str(subprocess.check_output(['''th ~/char-rnn/sample.lua ~/char-rnn/cv/%s -primetext "%s:" -temperature %s'''%(checkpoint,word,temperature)],shell=True))
 response = response[response.rfind(word):]
 response = response[:response.find("\\n")]
 print(response)
 make=input("Write this to html file? (y/n): ")
 if "y" in make:
-	with open("html/urb.htm","r") as template:
+	with open("backgroundtools/urb.htm","r") as template:
 		temp=template.read()
 	temp=temp.replace("INPUTTERM123",word)
 	temp=temp.replace("INPUTDEF123",response[response.find(":")+2:])
@@ -29,5 +29,5 @@ if "y" in make:
 	longd=now.strftime("%B %d, %Y")
 	temp=temp.replace("INPUTSHORTDATE123",short)
 	temp=temp.replace("INPUTDATE123",longd)
-	with open("html/%s.html"%(word),"w") as new:
+	with open("backgroundtools/%s.html"%(word),"w") as new:
 		new.write(temp)
